597f0272e2b3d434be5391d21884b5f21a2fab1c
Stanislav Rakitov <prosperousrf@gmail.com>
2022-11-27 17:43:23 +0300

    Описание проекта

--- src/main/java/com/rakitov/moneytransferservice/MoneytransferserviceApplication.java
+++ src/main/java/com/rakitov/moneytransferservice/MoneytransferserviceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MoneytransferserviceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MoneytransferserviceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MoneytransferserviceApplication.class, args);
+    }
 
 }
